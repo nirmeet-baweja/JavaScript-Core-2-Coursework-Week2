18a212ccb291a878a079a690802d6f6fdcf27726
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2021-01-18 19:21:46 +0000

    Exercise A Completed

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,16 @@
-function listOfNames(arrayOfPeople) {
-  let content = document.querySelector("#content");
+function listOfNames(people) {
+  let content = document.getElementById("content");
+
+  for(let i = 0; i < people.length; i++) {
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h2");
+
+    h1.innerHTML = people[i].name;
+    h2.innerHTML = people[i].job;
+
+    content.appendChild(h1);
+    content.appendChild(h2);
+  }
 }
 
 let people = [
