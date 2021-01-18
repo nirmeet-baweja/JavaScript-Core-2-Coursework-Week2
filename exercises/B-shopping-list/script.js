70bdb07a77dfd623a762df0d2b670e7272573cd4
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2021-01-18 19:31:15 +0000

    Exercise B Completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(list) {
+  let content = document.getElementById("content");
+
+  let ul = document.createElement("ul");
+
+  for(let i = 0; i < list.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = list[i];
+    ul.appendChild(listItem);
+  }
+
+  content.appendChild(ul);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
